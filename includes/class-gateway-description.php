622691e4ef7afec2d23157ddd6164b909ce8eb50
toobiii <toby.arnold@firemind.io>
2021-05-24 22:42:29 +0100

    Add if statement

--- includes/class-gateway-description.php
+++ includes/class-gateway-description.php
@@ -3,15 +3,18 @@
 add_filter( 'woocommerce_gateway_description', 'mode_gateway_description_fields', 20, 2 );
 
 function mode_gateway_description_fields( $description, $payment_id ) {
-	ob_start();
+	if ($payment_id === 'mode_gateway') {
+		ob_start();
 
-	echo '<center>'; ?>
+		echo '<center>'; ?>
 
-	<img src="<? echo plugin_dir_url( __FILE__ ).'assets/logo.png' ?>">
-<?  echo '<h5>You are almost there!</h5>';
-		echo '<p>After clicking "Place Order" you will use the Mode App to complete your purchase ⚡.</p>';
-	echo '</center>';
+		<img src="<? echo plugin_dir_url( __FILE__ ).'assets/logo.png' ?>">
+		<?
+	    echo '<h5>You are almost there!</h5>';
+			echo '<p>After clicking "Place Order" you will use the Mode App to complete your purchase ⚡.</p>';
+		echo '</center>';
 
-	$description .= ob_get_clean();
-	return $description;
+		$description .= ob_get_clean();
+		return $description;
+	}
 }
