379c87f9983a42049ab956cdb27cacf0f4b2b49a
toobiii <toby.arnold@firemind.io>
2021-05-24 17:24:50 +0100

    Update logo

--- includes/class-gateway-description.php
+++ includes/class-gateway-description.php
@@ -5,9 +5,10 @@ add_filter( 'woocommerce_gateway_description', 'mode_gateway_description_fields'
 function mode_gateway_description_fields( $description, $payment_id ) {
 	ob_start();
 
-	echo '<center>';
-		echo '<img src="'.plugin_dir_url( __FILE__ ) . "images/logo.png"'>';
-		echo '<h5>You are almost there!</h5>';
+	echo '<center>'; ?>
+
+	<img src="<? echo plugin_dir_url( __FILE__ )."images/logo.png" ?> >
+<?  echo '<h5>You are almost there!</h5>';
 		echo '<p>After clicking "Place Order" you will use the Mode App to complete your purchase ⚡.</p>';
 	echo '</center>';
 
