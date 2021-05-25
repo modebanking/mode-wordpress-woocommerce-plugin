7d50c42863f5c61bc92df0c73f57f68b301912db
toobiii <toby.arnold@firemind.io>
2021-05-25 13:31:53 +0100

    Update description

--- includes/class-gateway-description.php
+++ includes/class-gateway-description.php
@@ -10,7 +10,7 @@ function mode_gateway_description_fields( $description, $payment_id ) {
 
 		<img src="<? echo plugin_dir_url( __FILE__ ).'assets/logo.png' ?>">
 		<?
-	    echo '<h5>You are almost there!</h5>';
+	    echo '<h5>You\'re almost there!</h5>';
 			echo '<p>After clicking "Place Order" you will use the Mode App to complete your purchase ⚡.</p>';
 		echo '</center>';
 
