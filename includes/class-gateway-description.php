abea17988ec7b6160278ed32a3ef6b17b696ca9e
toobiii <toby.arnold@firemind.io>
2021-05-25 13:48:39 +0100

    Fixed

--- includes/class-gateway-description.php
+++ includes/class-gateway-description.php
@@ -5,11 +5,14 @@ add_filter( 'woocommerce_gateway_description', 'mode_gateway_description_fields'
 function mode_gateway_description_fields( $description, $payment_id ) {
 	if ($payment_id === 'mode_gateway') {
 		ob_start();
-		echo '<center>'; ?>
-		<img src="<? echo plugin_dir_url( __FILE__ ).'assets/logo.svg' ?>">
-		<? echo '<h5>You\'re almost there!</h5>';
-			echo '<p>After clicking "Place Order" you will use the Mode App to complete your purchase ⚡.</p>';
-		echo '</center>';
+
+		echo '<div>';
+			echo '<center>'; ?>
+			<img src="<? echo plugin_dir_url( __FILE__ ).'assets/logo.svg' ?>">
+			<? echo '<h5>You\'re almost there!</h5>';
+				echo '<p>After clicking "Place Order" you will use the Mode App to complete your purchase ⚡.</p>';
+			echo '</center>';
+		echo '</div>';
 
 		$description .= ob_get_clean();
 		return $description;
