5369b900a12ae76bedb2a9d59cd00871f82fb71c
toobiii <toby.arnold@firemind.io>
2021-05-25 13:36:45 +0100

    Update spacing

--- includes/class-gateway-description.php
+++ includes/class-gateway-description.php
@@ -7,10 +7,8 @@ function mode_gateway_description_fields( $description, $payment_id ) {
 		ob_start();
 
 		echo '<center>'; ?>
-
-		<img src="<? echo plugin_dir_url( __FILE__ ).'assets/logo.png' ?>">
-		<?
-	    echo '<h5>You\'re almost there!</h5>';
+		<img src="<? echo plugin_dir_url( __FILE__ ).'assets/logo.svg' ?>">
+		<? echo '<h5>You\'re almost there!</h5>';
 			echo '<p>After clicking "Place Order" you will use the Mode App to complete your purchase ⚡.</p>';
 		echo '</center>';
 
