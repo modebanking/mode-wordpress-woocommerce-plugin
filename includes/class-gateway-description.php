11ed62ada94306517734e01100f669054b4a4449
toobiii <toby.arnold@firemind.io>
2021-05-25 10:18:32 +0100

    Gateway

--- includes/class-gateway-description.php
+++ includes/class-gateway-description.php
@@ -21,7 +21,7 @@ function mode_gateway_description_fields( $description, $payment_id ) {
 
 add_filter( 'woocommerce_gateway_icon', 'mode_gateway_description_logo', 19, 2);
 
-function mode_gateway_description_logo($icon, $payment_id) {
+function mode_gateway_description_logo( $icon, $payment_id ) {
 	if ($payment_id === 'mode_gateway') {
 	?>
 	<img alt="Mode App logo" src="<? echo plugin_dir_url( __FILE__ ).'assets/logo.png' ?>">
