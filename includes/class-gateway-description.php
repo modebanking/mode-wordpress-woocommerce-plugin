cb4b12c8e3a531f1bd6c0fe5e66dcb63beb2fc1c
toobiii <toby.arnold@firemind.io>
2021-05-25 13:38:52 +0100

    Remove spacing

--- includes/class-gateway-description.php
+++ includes/class-gateway-description.php
@@ -5,7 +5,6 @@ add_filter( 'woocommerce_gateway_description', 'mode_gateway_description_fields'
 function mode_gateway_description_fields( $description, $payment_id ) {
 	if ($payment_id === 'mode_gateway') {
 		ob_start();
-
 		echo '<center>'; ?>
 		<img src="<? echo plugin_dir_url( __FILE__ ).'assets/logo.svg' ?>">
 		<? echo '<h5>You\'re almost there!</h5>';
