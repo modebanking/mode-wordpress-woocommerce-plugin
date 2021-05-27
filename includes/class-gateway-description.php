301e1cb5e67fb6faac0648f9a9911cf167198e62
toobiii <toby.arnold@firemind.io>
2021-05-27 09:38:08 +0100

    Removr the logo

--- includes/class-gateway-description.php
+++ includes/class-gateway-description.php
@@ -18,15 +18,3 @@ function mode_gateway_description_fields( $description, $payment_id ) {
 		return $description;
 	}
 }
-
-add_filter( 'woocommerce_gateway_icon', 'mode_gateway_description_logo', 19, 2);
-
-function mode_gateway_description_logo( $icon, $payment_id ) {
-	if ($payment_id === 'mode_gateway') {
-	?>
-	<img alt="Mode App logo" src="<? echo plugin_dir_url( __FILE__ ).'assets/logo.png' ?>">
-	<?
-	} else {
-		return $icon;
-	}
-}
