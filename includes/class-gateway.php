738017d9e68d1d9b6524d7c31756a693ac18f326
toobiii <toby.arnold@firemind.io>
2021-06-09 11:07:22 +0100

    Update urls

--- includes/class-gateway.php
+++ includes/class-gateway.php
@@ -32,9 +32,9 @@ class Mode_Gateway extends WC_Payment_Gateway {
 	const HC_TYPE_REDIRECT = 'redirect';
 	const HC_TYPE_MODAL = 'modal';
 
-	const AUTH_URL = 'https://dev-mode.eu.auth0.com/oauth/token';
-	const API_CALLBACK_URL = 'https://4krsfra6y4.execute-api.eu-west-2.amazonaws.com/qa1/merchants/callbacks';
-	const API_SIGNATURE_URL = 'https://4krsfra6y4.execute-api.eu-west-2.amazonaws.com/qa1/merchants/payments/sign';
+	const AUTH_URL = 'https://auth.modeapp.com/oauth/token';
+	const API_CALLBACK_URL = 'https://hpxjxq5no8.execute-api.eu-west-2.amazonaws.com/production/merchants/callbacks';
+	const API_SIGNATURE_URL = 'https://hpxjxq5no8.execute-api.eu-west-2.amazonaws.com/production/merchants/payments/sign';
 
 	/**
 	 * @var Mode_GatewayService
@@ -123,9 +123,10 @@ class Mode_Gateway extends WC_Payment_Gateway {
 		);
 
 		$context = stream_context_create($options);
-		$result = json_decode(file_get_contents('https://4krsfra6y4.execute-api.eu-west-2.amazonaws.com/qa1/merchants/payments/'.$paymentId, false, $context));
+		$result = json_decode(file_get_contents('https://hpxjxq5no8.execute-api.eu-west-2.amazonaws.com/production/merchants/payments/'.$paymentId, false, $context));
 		$userId = $result->userId;
 		$currency = $order->get_currency();
+		$currencySymbol = get_woocommerce_currency_symbol($currency);
 
 		$requestDataRefund = array(
 			'userId' => $userId,
@@ -150,14 +151,14 @@ class Mode_Gateway extends WC_Payment_Gateway {
 
 		$context = stream_context_create($options);
 
-		$getFileRequest = file_get_contents('https://4krsfra6y4.execute-api.eu-west-2.amazonaws.com/qa1/merchants/payments/refunds', false, $context);
+		$getFileRequest = file_get_contents('https://hpxjxq5no8.execute-api.eu-west-2.amazonaws.com/production/merchants/payments/refunds', false, $context);
 		$result = json_decode($getFileRequest);
 
 		if ($result->error) {
 			$order->add_order_note('Refund unsuccessful. This user has already been refunded.');
 			return false;
 		} else {
-			$order->add_order_note('Refund successful. Refunded '.$amount.' '.$currency.' to Mode user '.$userId);
+			$order->add_order_note('Refund successful. Refunded '.$currencySymbol.$amount.' to Mode user '.$userId);
 			return true;
 		}
 	}
