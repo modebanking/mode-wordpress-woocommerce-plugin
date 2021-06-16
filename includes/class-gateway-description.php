d32a2438dfa7a923433671bf0f1d1a594afa738d
toobiii <toby.arnold@firemind.io>
2021-06-16 15:25:36 +0100

    Fix file stuff

--- includes/class-gateway-description.php
+++ includes/class-gateway-description.php
@@ -6,10 +6,12 @@ function mode_gateway_description_fields( $description, $payment_id ) {
 	if ($payment_id === 'mode_gateway') {
 		ob_start();
 
+		$filepath = plugin_dir_url( __FILE__ ).'assets/logo.svg';
+
 		echo '<div>';
-			echo '<center>'; ?>
-			<img src="<? echo plugin_dir_url( __FILE__ ).'assets/logo.svg' ?>">
-			<? echo '<h4>You\'re nearly there!</h4>';
+			echo '<center>';
+				echo '<img alt="Mode Logo" src="'.$filepath.'">';
+				echo '<h4>You\'re nearly there!</h4>';
 				echo '<p>After clicking "Place Order" you will use the Mode App to complete your purchase ⚡.</p>';
 			echo '</center>';
 		echo '</div>';
@@ -18,3 +20,4 @@ function mode_gateway_description_fields( $description, $payment_id ) {
 		return $description;
 	}
 }
+?>
